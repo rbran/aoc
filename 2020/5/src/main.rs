d91971d75feb3d99cb2ed5ddae78369ae0722a71
Rubens Brandao <906c189d@opayq.com>
2020-12-05 14:57:31 -0300

    solved 2020 day 5 part 2

--- 2020/5/src/main.rs
+++ 2020/5/src/main.rs
@@ -10,6 +10,13 @@ fn main() {
         .unwrap();
     let file = fs::read_to_string(file_path).expect("Unable to read the File");
 
+    //At first, I didn't understood exacly what the AoC meant by
+    //"some of the seats at the very front and back of the plane"
+    //so I did this, but was too lazy to improve it, maybe in the future
+    let mut dodge_this = Vec::with_capacity(1024);
+    for i in 0u16..1024 {
+        dodge_this.push(i);
+    }
     let mut p1_res = 0u16;
     for line in file.lines() {
         let mut id = 0u16;
@@ -25,6 +32,17 @@ fn main() {
         if id > p1_res {
             p1_res = id;
         }
+        dodge_this.remove(dodge_this.binary_search(&id).unwrap());
     }
     println!("P1: {}", p1_res);
+    //println!("Smith: {:?}", dodge_this);
+    //we should get a vector with the format [0,1,2,...,56,456,667,668...]
+    //and 456 is our number, so just check when the index is diferent
+    //from the value, the first time that happen, we got out number
+    let (_, p2_res) = dodge_this
+        .iter()
+        .enumerate()
+        .find(|(i, &v)| *i != v as usize)
+        .unwrap();
+    println!("P2: {}", p2_res);
 }
