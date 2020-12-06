85aad6d69413e39e2cbae9b58cb700215fd41e50
Rubens Brandao <906c189d@opayq.com>
2020-12-06 07:26:50 -0300

    solved 2020 day 6 part 2

--- 2020/6/src/main.rs
+++ 2020/6/src/main.rs
@@ -28,15 +28,28 @@ fn main() {
     });
 
     let mut p1_res = 0usize;
+    let mut p2_res = 0usize;
     for group in groups {
+        let mut num_person = 0usize;
         let mut group_answers: HashMap<u8, usize> = HashMap::new();
         for person in group.split(|&x| x == b'\n') {
+            //bug: this happen because the last line end with \n
+            if person.len() == 0 {
+                continue;
+            }
+            num_person += 1;
             for &answer in person {
                 let entry = group_answers.entry(answer).or_insert(0);
                 *entry += 1;
             }
         }
         p1_res += group_answers.len();
+        for v in group_answers.values() {
+            if *v == num_person {
+                p2_res += 1;
+            }
+        }
     }
     println!("P1: {}", p1_res);
+    println!("P2: {}", p2_res);
 }
