df6455026338b0e73dd589c7b3a23c8774d4e2eb
Rubens Brandao <906c189d@opayq.com>
2020-12-07 16:44:13 -0300

    improved 2020 day 2, small improvements in readability

--- 2020/2/src/main.rs
+++ 2020/2/src/main.rs
@@ -2,44 +2,34 @@ use regex::Regex;
 use std::env;
 use std::fs;
 
-const DEFAULT_INPUT_FILE: &str = "input.txt";
-
-fn main() {
+fn main() -> Result <(), Box<dyn std::error::Error>> {
     // assuming all passwords are lower case
     let regex = Regex::new("([0-9]+)-([0-9]+) ([a-z]): ([a-z]+)").unwrap();
-
-    let file_path = env::args()
-        .nth(1)
-        .or(Some(DEFAULT_INPUT_FILE.to_string()))
-        .unwrap();
-    let file = fs::read_to_string(file_path).expect("Unable to read the File");
+    let input = fs::read_to_string(env::args().nth(1).unwrap_or("input.txt".to_string()))?;
     let mut p1_valid_count = 0; //part 1 counter
     let mut p2_valid_count = 0; //part 2 counter
-    for cap in regex.captures_iter(file.as_str()) {
+    for cap in regex.captures_iter(&input) {
         let (min, max, chr, pass) = (
-            cap[1].parse::<usize>().unwrap(),
-            cap[2].parse::<usize>().unwrap(),
-            cap[3].parse::<char>().unwrap(),
+            cap[1].parse::<usize>()?,
+            cap[2].parse::<usize>()?,
+            cap[3].parse::<char>()?,
             cap[4].to_string(),
         );
         assert!(min <= max);
         // check part 1
-        let mut chr_count = 0;
-        for i in pass.chars() {
-            if i == chr {
-                chr_count += 1;
-            }
-        }
+        let chr_count = pass.chars().filter(|&x| x == chr).count();
         if chr_count <= max && chr_count >= min {
             p1_valid_count += 1;
         }
         //check part 2
         let mut pass_iter = pass.chars();
-        let first = pass_iter.nth(min - 1).unwrap();
-        let second = pass_iter.nth(max - min - 1).unwrap(); //reuse the same iterator
-        p2_valid_count += if first == second { 0 }
-        else if first == chr || second == chr { 1 }
-        else { 0 };
+        let first = pass_iter.nth(min - 1);
+        let second = pass_iter.nth(max - min - 1); //reuse the same iterator
+
+        if (first == Some(chr)) ^ (second == Some(chr)) {
+            p2_valid_count += 1
+        }
     }
     println!("p1 {}\np2 {}", p1_valid_count, p2_valid_count);
+    Ok(())
 }
