ca1a4dc742028926d3cf47e22069744111aac116
Rubens Brandao <906c189d@opayq.com>
2020-12-15 10:25:14 -0300

    solved 2020 day 15 part 2

--- 2020/15/src/main.rs
+++ 2020/15/src/main.rs
@@ -38,7 +38,31 @@ fn solve1(input: &str) -> Result<usize, Box<Error>> {
 }
 
 fn solve2(input: &str) -> Result<usize, Box<Error>> {
-    unimplemented!();
+    // same as solve 1, but optimized to reduze memory use
+    // I should think better about this, probably can optimize it greatly.
+    // although this works :)
+    let mut game: HashMap<usize, (Option<usize>, Option<usize>)> = HashMap::new();
+    let input = parse(input)?;
+    for (i, v) in input.iter().enumerate() {
+        game.insert(*v, (Some(i), None));
+    }
+    let mut last_number = input.last().copied().unwrap();
+    for turn in input.len()..30000000 {
+        // calculate the number to speak
+        let indexes = game.get(&last_number).unwrap();
+        let next_number = match indexes {
+            // the last number happend twice or more
+            (Some(x), Some(y)) => *x - *y,
+            // the last number only happend once
+            _ => 0,
+        };
+        // insert the next number to the game (speak it)
+        let entry = game.entry(next_number).or_insert((None, None));
+        entry.1 = entry.0;
+        entry.0 = Some(turn);
+        last_number = next_number;
+    }
+    Ok(last_number)
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -61,6 +85,23 @@ fn test_part_1() -> Result<(), Box<dyn std::error::Error>> {
     ];
     for (input, result) in INPUTS {
         assert_eq!(solve1(input)?, *result);
+    }
+    Ok(())
+}
+
+#[test]
+fn test_part_2() -> Result<(), Box<dyn std::error::Error>> {
+    const INPUTS: &[(&str, usize)] = &[
+        ("0,3,6", 175594),
+        ("1,3,2", 2578),
+        ("2,1,3", 3544142),
+        ("1,2,3", 261214),
+        ("2,3,1", 6895259),
+        ("3,2,1", 18),
+        ("3,1,2", 362),
+    ];
+    for (input, result) in INPUTS {
+        assert_eq!(solve2(input)?, *result);
     }
     Ok(())
 }
