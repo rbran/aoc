a62b6d281d474d78813ade043943759845127ab5
Rubens Brandao <906c189d@opayq.com>
2020-12-16 14:47:20 -0300

    solved 2020 day 16 part 2

--- 2020/16/src/main.rs
+++ 2020/16/src/main.rs
@@ -1,3 +1,7 @@
+//I HATE everthing about this code, this is pure shit.
+//But I was too tired to fix anything this day.
+//Don't read bellow this line unless you are very motivated to fix lots of shit
+
 use std::collections::HashMap;
 use std::env;
 use std::fs;
@@ -122,8 +126,164 @@ fn solve1(input: &str) -> Result<usize, Box<Error>> {
     Ok(ret)
 }
 
+fn find_valid_rule(field: usize, rules: &Vec<Rule>) -> Option<usize> {
+    rules.iter().enumerate().find_map(|(i, rule)| {
+        if (field >= rule.range1.0 && field <= rule.range1.1)
+            || (field >= rule.range2.0 && field <= rule.range2.1) {
+            Some(i)
+        } else {
+            None
+        }
+    })
+}
+
+// TODO: Return Iterator
+fn find_all_valid_rules(field: usize, rules: &Vec<Rule>) -> Vec<usize> {
+    rules.iter().enumerate().filter_map(|(i, rule)| {
+        if (field >= rule.range1.0 && field <= rule.range1.1)
+            || (field >= rule.range2.0 && field <= rule.range2.1) {
+            Some(i)
+        } else {
+            None
+        }
+    }).collect()
+}
+
+fn and_vector(a: &mut Vec<usize>, b: &Vec<usize>) {
+    let mut ret = Vec::new();
+    for i in a.iter() {
+        if a.contains(i) && b.contains(i) {
+            ret.push(*i);
+        }
+    }
+    a.clear();
+    a.extend(ret);
+}
+
 fn solve2(input: &str) -> Result<usize, Box<Error>> {
-    unimplemented!();
+    let input = input.parse::<Input>()?;
+    let rules = input.rules;
+    // remove invalid tickets
+    let mut sudoku = Vec::new();
+    sudoku.push(&input.your_ticket);
+    for ticket in input.nearby_tickets.iter() {
+        let first_valid = ticket.iter().find(|&field| {
+            find_valid_rule(*field, &rules).is_none()
+        });
+        if first_valid.is_none() {
+            sudoku.push(ticket);
+        }
+    }
+
+    // prepare the sudoku shit
+    let paper_lines = sudoku.len();
+    let paper_columns = rules.len();
+    //possible rules of each column
+    let mut paper: Vec<Vec<Vec<usize>>> = vec![];
+    for line in sudoku {
+        let mut paper_line = vec![];
+        for (c, field) in line.iter().enumerate() {
+            let valid = find_all_valid_rules(*field, &rules);
+            paper_line.push(valid);
+        }
+        paper.push(paper_line);
+    }
+
+    // do sudoku shit
+    // values found
+    let mut res: Vec<Option<usize>> = vec![None; rules.len()];
+    loop {
+        //clean the paper
+        for &found in res.iter() {
+            let found = match found {
+                None => continue,
+                Some(found) => found,
+            };
+            for line in 0..paper_lines {
+                for col in 0..paper_columns {
+                    if res[col].is_some() {
+                        continue;
+                    }
+                    let remove = paper[line][col].iter().enumerate().find(|(i, x)| **x == found);
+                    match remove {
+                        None => (),
+                        Some((i, _)) => {paper[line][col].remove(i);},
+                    }
+                }
+            }
+        }
+
+        let mut change = false;
+
+        //apply AND to all the columns
+        for col in 0..paper_columns {
+            if res[col].is_some() {
+                continue;
+            }
+            let mut new_col = paper[0][col].clone();
+            for line in 1..paper_lines {
+                and_vector(&mut new_col, &paper[line][col]);
+            }
+            if new_col.len() == 1 {
+                //found the solution for the col
+                change = true;
+                *res.get_mut(col).unwrap() = new_col.get(0).copied();
+                for line in 1..paper_lines {
+                    paper[line][col].clear();
+                }
+            } else {
+                for line in 1..paper_lines {
+                    if paper[line][col].len() != new_col.len() {
+                        change = true;
+                        *paper[line].get_mut(col).unwrap() = new_col.clone();
+                    }
+                }
+            }
+        }
+        if change {
+            continue; //clean and do again
+        }
+        change = false;
+
+        //check if a line have only one rule that a apply to only one column
+        for line in 0..paper_lines {
+            let mut count = HashMap::new();
+            for col in 0..paper_columns {
+                if res[col].is_some() {
+                    continue;
+                }
+                for field in paper[line][col].iter() {
+                    *count.entry(field).or_insert(0) += 1;
+                }
+            }
+            //check if a rule can only be applied in one place
+            for (_, v) in count.iter() {
+                if *v == 1 {
+                    let (col, _) = paper[line].iter().enumerate().find(|(i, x)| x.iter().find(|y| **y == *v).is_some()).unwrap();
+                    change = true;
+                    *res.get_mut(col).unwrap() = Some(*v);
+                }
+            }
+        }
+
+        if change {
+            continue;
+        } else {
+            break
+        }
+    }
+    println!("res {:?}", res);
+    let mut easy_bro = Vec::new();
+    for (index, fuck_this) in res.iter().enumerate() {
+        if fuck_this.unwrap() < 6 {
+            easy_bro.push(index);
+        }
+    }
+    let mut res = 1;
+    for easy in easy_bro {
+        res *= input.your_ticket[easy];
+    }
+    Ok(res)
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
